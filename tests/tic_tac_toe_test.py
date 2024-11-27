84f1bfe16271e021ed3c785c39f1bc15718ed4aa
Matthew Manalang <matt.manalang@gmail.com>
2024-11-27 10:08:33 -0600

    Implement TicTacToe.check_board and the respective unit tests

--- tests/tic_tac_toe_test.py
+++ tests/tic_tac_toe_test.py
@@ -12,6 +12,7 @@ class TestTicTacToe(unittest.TestCase):
     def test_init_game(self):
         """Initializing to an empty game board/state."""
         game = TicTacToe()
+        self.assertEqual(game.current_player, 1)
         self.assertEqual(game.get_game_state(), [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']])
 
     def test_one_move(self):
@@ -20,10 +21,18 @@ class TestTicTacToe(unittest.TestCase):
         game.place_piece("b2")
         self.assertEqual(game.get_game_state(), [['-', '-', '-'], ['-', '-', 'X'], ['-', '-', '-']])
 
+    def test_player_change(self):
+        """Check that the current player (1 or 2) is getting set properly"""
+        game = TicTacToe()
+        self.assertEqual(game.current_player, 1)
+        game.next_player()
+        self.assertEqual(game.current_player, 2)
+
     def test_two_moves(self):
         """Check the state of the game after Player 1 & 2 make their first moves."""
         game = TicTacToe()
         game.place_piece("b2")
+        game.next_player()
         game.place_piece("a0")
         self.assertEqual(game.get_game_state(), [['O', '-', '-'], ['-', '-', 'X'], ['-', '-', '-']])
 
@@ -31,10 +40,41 @@ class TestTicTacToe(unittest.TestCase):
         """Check the state of the game after Player 2 tries to claim Player 1's cell."""
         game = TicTacToe()
         game.place_piece("b2")
+        game.next_player()
         game.place_piece("a0")
         status = "BAD" if not game.place_piece("a0") else "OK"
         self.assertEqual(status, "BAD")
         self.assertEqual(game.get_game_state(), [['O', '-', '-'], ['-', '-', 'X'], ['-', '-', '-']])
+
+    def test_winner_easy(self):
+        """Check the winning condition. In this test, Player 1 (X) will be the winner."""
+        # Excluding game.next_player() to simplify the test
+        game = TicTacToe()
+        # Straight across the top row
+        game.place_piece("a0")
+        game.place_piece("a1")
+        game.place_piece("a2")
+        self.assertEqual(game.check_board(), 1)
+
+    def test_winner_normal(self):
+        """Check the winning condition. In this test, alternate between Player 1 and 2 normally."""
+        game = TicTacToe()
+        game.place_piece("a0")
+        game.next_player()
+        game.place_piece("a1")
+        game.next_player()
+        game.place_piece("b1")
+        game.next_player()
+        game.place_piece("a2")
+        game.next_player()
+        game.place_piece("c2")
+        self.assertEqual(game.check_board(), 1)
+
+    def test_no_winner(self):
+        """Check the condition in which neither player wins. All spaces have been used up."""
+        game = TicTacToe()
+        game.board = [["X", "O", "X"], ["X", "O", "O"], ["O", "O", "X"]]
+        self.assertEqual(game.check_board(), -1)
 
 # Use only if you are testing directly from running this file.
 if __name__ == "__main__":
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,13 @@
 """Module that contains the game logic for a text-based Tic-Tac-Toe"""
+
+
 class TicTacToe:
     """Creates a TicTacToe object that manages a single instance of a game."""
+
     def __init__(self) -> None:
-        self.board = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
-        self.row_map = {'a': 0, 'b': 1, 'c': 2}
+        self.board = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+        self.row_map = {"a": 0, "b": 1, "c": 2}
+        self.player_map = {1: "X", 2: "O"}
         self.current_player = 1  # Possible values: [1, 2]
 
     def get_game_state(self):
@@ -12,7 +16,7 @@ class TicTacToe:
 
     def display_board(self):
         """Displays the current state of the game board"""
-        output = f"""   1     2     3
+        output = f"""   0     1     2
       |     |     
 a  {self.board[0][0]}  |  {self.board[0][1]}  |  {self.board[0][2]}  
  _____|_____|_____
@@ -24,31 +28,78 @@ c  {self.board[2][0]}  |  {self.board[2][1]}  |  {self.board[2][2]}
       |     |     """
         print(output)
 
+    def next_player(self):
+        """Set the current player to the next player."""
+        self.current_player = 1 if self.current_player == 2 else 2
+
     def check_board(self):
-        """Check the board to see if there are any winning moves."""
+        """Check the board to see if the current player has made a winning move."""
+        target_piece = self.player_map.get(self.current_player)
+
+        # Check patterns that start with the a0 cell
+        if self.board[0][0] == target_piece:
+            if (
+                (self.board[0][0] == self.board[0][1] == self.board[0][2])  # a0 to a2
+                or (self.board[0][0] == self.board[1][0] == self.board[2][0])  # a0 to c0
+                or (self.board[0][0] == self.board[1][1] == self.board[2][2])  # a0 to c2
+            ):
+                return self.current_player
+
+        # Check pattern(s) that start with the a1 cell
+        if self.board[0][1] == target_piece:
+            if self.board[0][1] == self.board[1][1] == self.board[2][1]:  # a1 to c1
+                return self.current_player
+
+        # Check pattern(s) that start with the a2 cell
+        if self.board[0][2] == target_piece:
+            if (
+                (self.board[0][2] == self.board[1][2] == self.board[2][2])
+                or (self.board[0][2] == self.board[1][1] == self.board[2][0])
+            ):
+                return self.current_player
+
+        # Check pattern(s) that start with the b0 cell
+        if self.board[1][0] == target_piece:
+            if self.board[1][0] == self.board[1][1] == self.board[1][2]:
+                return self.current_player
+
+        # Check pattern(s) that start with the c0 cell
+        if self.board[2][0] == target_piece:
+            if self.board[2][0] == self.board[2][1] == self.board[2][2]:
+                return self.current_player
+
+        # No winning patterns that match
+        return -1
 
     def is_valid_move(self, input_string):
         """Validate that the user inputs a {row letter}{column number} for their input string
         and that the move is doable."""
-        if (len(input_string) == 2) and (input_string[0] in self.row_map) and (int(input_string[-1]) in self.row_map.values()):
+        if (
+            (len(input_string) == 2)
+            and (input_string[0] in self.row_map)
+            and (int(input_string[-1]) in self.row_map.values())
+        ):
             target = (self.row_map.get(input_string[0]), int(input_string[1]))
-            if self.board[target[0]][target[1]] != '-':  # Cell has already been claimed
+            if self.board[target[0]][target[1]] != "-":  # Cell has already been claimed
                 return False
             # Cell has not already been claimed
             return True
-        
+
         # Invalid Move
         return False
-
 
     def place_piece(self, location_string):
         """Places an 'X' or 'O' on the board using the format {row letter}{column number}.
         Example: b2"""
         # Do stuff to the game board if doable move
         if self.is_valid_move(location_string):
-            valid_target = (self.row_map.get(location_string[0]), int(location_string[1]))
-            self.board[valid_target[0]][valid_target[1]] = 'X' if self.current_player == 1 else 'O'
-            self.current_player = 1 if self.current_player == 2 else 2
+            valid_target = (
+                self.row_map.get(location_string[0]),
+                int(location_string[1]),
+            )
+            self.board[valid_target[0]][valid_target[1]] = self.player_map.get(
+                self.current_player
+            )
             return True
 
         # TODO: Do something when the move isn't valid.
